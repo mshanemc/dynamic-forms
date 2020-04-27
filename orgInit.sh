131cd9975bad7b007880c69cfec10c293dc04956
mshanemc <shane.mclaughlin@salesforce.com>
2020-04-27 09:17:27 -0500

    plus vehicle stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,6 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 60
 sfdx force:source:push
+sfdx force:user:permset:assign -n AppBuilder
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
+sfdx automig:load -d data
