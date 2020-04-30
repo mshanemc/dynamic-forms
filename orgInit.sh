b1445d0a3f2929d5e057a9318796b1af2313cced
mshanemc <shane.mclaughlin@salesforce.com>
2020-04-30 11:04:12 -0500

    no reason to do the password thing on this repo

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n AppBuilder
-sfdx shane:user:password:set -g User -l User -p salesforce1
-sfdx force:org:open
+sfdx force:org:open -p /lightning/o/Vehicle__c/list?filterName=Recent
 sfdx automig:load -d data --concise
