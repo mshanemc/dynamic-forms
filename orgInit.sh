69646d2f9151a64f27585495cd7fac1b2e6b4188
mshanemc <shane.mclaughlin@salesforce.com>
2020-08-18 17:28:52 -0500

    15 day orgs

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 60
+sfdx force:org:create -f config/project-scratch-def.json -s -d 15 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n AppBuilder
 sfdx force:org:open -p /lightning/o/Vehicle__c/list?filterName=Recent
